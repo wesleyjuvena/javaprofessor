04a74d6768741ee20268384c717913e1667db116
Miguel <1miguelarthur@gmail.com>
2024-05-03 21:42:27 -0300

    javaprofessor
    esse e o java do professor

---
+++ script.js
@@ -0,0 +1,27 @@
+const video = document.getElementById("myVideo");  //Estamos fazendo uma busca dentro do HTML para salvar o ID
+const btn = document.getElementById("myBtn");
+
+
+
+function myFunction() { //Aqui criamos uma Função 
+  
+  
+if (video.paused) { //se o vídeo estiver pausado execute: Play.
+    video.play();
+    btn.innerHTML = "Pause"; // O innerHTML funciona como uma alteração textual dentro do HTML, usamos ele no Javascript para executar essa chamada.
+  } else { //caso contrário, se o 
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+window.revelar = ScrollReveal({reset:true});
+
+
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 3000,
+    distance: '150px'
+});
